ed7544900138c11584e35d1739507e15064e36ad
Vidya Sagar <vidya_s@blr.trigent.com>
2022-05-18 13:07:39 +0530

    Some Changes In the List

--- src/Components/List.js
+++ src/Components/List.js
@@ -2,7 +2,7 @@ import React,{useState,useEffect} from 'react';
 import {AiOutlineSearch} from 'react-icons/ai';
 import axios from 'axios';
 import {NavLink} from 'react-router-dom';
-import Footer from './Footer';
+import Footer from './Footer'
 
 function List() {
     const [APIData, setAPIData] = useState([]);
